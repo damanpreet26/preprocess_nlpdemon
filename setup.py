defe4f6df3749be80eda74641cde225f333255e6
damanpreet26 <damanpreets26@gmail.com>
2021-02-08 21:26:39 +0530

    code update

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import setuptools
-with open("README.md", "r") as file:
-    long_description=file.read()
+
+with open('README.md', 'r') as file:
+	long_description = file.read()
 
 
 setuptools.setup(
-    name = "preprocess_nlpdemon", 
-    version = "1.0",
-    author = "Damanpreet Dahele",
-    author_email = "damanpreets26@gmail.com",
-    description = "This is nlp pre-processing package",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    packages = setuptools.find_packages,
-    classifiers = [
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ],
-    python_requires = ">=3.5"
-)
+	name = 'preprocess_nlpdemon', #this should be unique
+	version = '0.0.1',
+	author = 'Damanpreet Singh Dahele"',
+	author_email = 'damanpreets26@gmail.com',
+	description = 'This is preprocessing package',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
