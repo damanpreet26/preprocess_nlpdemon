2187eab8a08b1bb08074d256c39b77b0414a70d6
damanpreet26 <damanpreets26@gmail.com>
2021-02-08 21:10:47 +0530

    Update utils.py

--- preprocess_nlpdemon/utils.py
+++ preprocess_nlpdemon/utils.py
@@ -12,14 +12,3 @@ import numpy as np
 def _get_word_counts(x):
     count=len(str(x).split())
     return count
-
-## character count
-def _char_counts(x):
-    count=len("".join(str(x).split()))
-    return count
-
-## average word length
-def _average_word_count(x):
-    val = _char_counts(x)/_get_word_counts(x)
-    return val
-
