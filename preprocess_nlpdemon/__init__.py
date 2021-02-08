7f33e7aa2bccac9e588bf802deb7d107d65504d7
damanpreet26 <damanpreets26@gmail.com>
2021-02-08 21:11:14 +0530

    Update __init__.py

--- preprocess_nlpdemon/__init__.py
+++ preprocess_nlpdemon/__init__.py
@@ -4,12 +4,4 @@ __version__="1.0"
 
 def get_word_counts(x):
 	return utils._get_word_counts(x)
-	
-
-def char_counts(x):
-	return utils._char_counts(x)
-
-
-def average_word_count(x):
-	return utils._average_word_count(x)
 
